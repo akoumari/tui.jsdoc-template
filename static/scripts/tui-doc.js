e2b7997d9d24c6741e8002c828d6d4fec0969312
Alex Koumarianos <56001723+akoumari@users.noreply.github.com>
2022-01-23 12:44:03 -0500

    Update tui-doc.js

--- static/scripts/tui-doc.js
+++ static/scripts/tui-doc.js
@@ -200,10 +200,10 @@ $lnb.find('.lnb-api').each(function() {
         let end = currentPath[currentPath.length-1].split(".")[0]
         let src = $(this).prev().attr("href");
         src = src?src.split(".html")[0]:"";
-      
+                   console.log("end"+end);
+        console.log("src"+src);
         if( src.indexOf(currentPath) != -1){
-             console.log(end);
-        console.log(src);
+
             $(this).next().toggleClass('hidden');
             
             
